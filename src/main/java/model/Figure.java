54b1f67c242f2881157f6eff21742fbe974887d5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-08-18 23:17:00 +0300

    finish Figure

--- src/main/java/model/Figure.java
+++ src/main/java/model/Figure.java
@@ -4,17 +4,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum Figure {
-    I1(0, 1,    1, 1,    2, 1,    3, 1),//oooo
+    I1(0, 1,   1, 1,   2, 1,   3, 1),//oooo
+
     I2(1, 0,
               1, 1,
               1, 2,
-              1, 3),//oooo
-    J,
-    L,
-    O,
-    S,
-    T,
-    Z;
+              1, 3),
+
+    J1(1, 0,
+              1, 1,
+              0, 2, 1, 2),
+
+    J2(0, 0,
+               0, 1,    1, 1,    2, 1),
+
+    J3( 1,0, 2,0,
+               1,1,
+               2,1),
+
+    J4(0, 1,    1, 1,    2, 1,
+                                2, 2),
+
+    L1(1,0,
+              1,1,
+              1,2,2,2),
+
+    L2(0,1,    1,1,    2,1,
+              0,2),
+
+    L3(0,0,   1,0,
+                      1,1,
+                      1,2),
+
+    L4(         2,0,
+         0,1,   1,1,   2,1),
+
+    O(0,0,   1,0,
+              0,1,   1,1),
+
+    S1(1,1,   2,1,
+      0,2,   1,2),
+
+    S2(0,0,
+              0,1,   1,1,
+                     1,2),
+
+    T1(0,1,   1,1,   2,1,
+                     1,2),
+    T2( 1,0,
+        0,1,   1,1,
+               1,2),
+
+    T3(          1,0,
+                 0,1,   1,1,   2,1),
+
+    T4(  1,0,
+                1,1,  2,1,
+                1,2),
+    Z1(0,1,   1,1,
+                      1,2,   2,2),
+
+    Z2(     2,0,
+            1,1,   2,1,
+            1,2);
 
     private List<Coord> dot;
 
@@ -23,5 +75,34 @@ public enum Figure {
         for (int i = 0; i < coords.length; i += 2) {
             dot.add(new Coord(coords[i], coords[i + 1]));
         }
+    }
+
+    public Figure turnRight() {
+        switch (this){
+            case I1:return I2;
+            case I2:return I1;
+            case J1:return J2;
+            case J2:return J3;
+            case J3:return J4;
+            case J4:return J1;
+            case L1:return L2;
+            case L2:return L3;
+            case L3:return L4;
+            case L4:return L1;
+            case O:return O;
+            case S1:return S2;
+            case S2:return S1;
+            case T1:return T2;
+            case T2:return T3;
+            case T3:return T4;
+            case T4:return T1;
+            case Z1:return Z2;
+            case Z2:
+            default:return Z1;
+        }
+    }
+
+    public Figure turnLeft() {
+        return turnRight().turnRight().turnRight();
     }
 }
